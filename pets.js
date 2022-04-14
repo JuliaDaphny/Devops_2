07bb5442936969e39d90be9a8704663935410395
JuliaDaphny <julialins96@gmail.com>
2022-04-14 18:58:57 -0300

    Commit final

--- pets.js
+++ pets.js
@@ -1,34 +1,92 @@
 const data = [
-    {id:1, nome:"Capitu", idade:15, sexo:"M", especie:"Calopisita"},
-    {id:2, nome:"Xuxu", idade:5, sexo:"M", especie:"Gato"},
-    {id:3, nome:"Mag", idade:10, sexo:"F", especie:"Galinha"},
-    {id:4, nome:"Zezinho", idade:11, sexo:"M", especie:"Tartaruga"},
-    {id:5, nome:"Gertrudi", idade:12, sexo:"F", especie:"Mula"},
-
+    {
+        id: 1, nome: "Lis",
+        idade: 15, sexo: "M",
+        especie: "bode"
+    },
+    {
+        id: 2, nome: "macos",
+        idade: 10, sexo: "M",
+        especie: "gato"
+    },
 ];
 
-function listar() {return data}
-function listarPorId(id) {let pet = data.find(pet => pet.id == id)
-     if(pet){return pet}else{return({erro: "True",mensagem:"Pet não encontrado!"})}}
-function cadastrar(x) {
-    x.id=data.length + 1
-    data.push(x);
-    return x
+function listar() {
+    return data;
 }
 
-function cadastrar() {return data}
-function listarPorId(id) {let pet = data.find(pet => pet.id == id)
-     if(pet){return pet}else{return({erro: "True",mensagem:"Pet não encontrado!"})}}
-function cadastrar(x) {
-    x.id=data.length + 1
-    data.push(x);
-    return x
+function listarPorId(id) {
+    const pet = data.find(pet => pet.id === id)
+    if (!pet) {
+        return {
+            erro: true,
+            mensagem: "Pet não encontrado!"
+        };
+    }
+    return pet;
 }
 
+function cadastrar(pet) {
+
+
+    if (pet.hasOwnProperty('nome') == false) {
+        return { erro: true, mensagem: "Campo 'nome' é obrigatório!" }
+    } else if(pet.hasOwnProperty('idade') == false){
+        return { erro: true, mensagem: "Campo 'idade' é obrigatório!" }
+
+    }
+    else if(pet.hasOwnProperty('sexo') == false){
+        return { erro: true, mensagem: "Campo 'sexo' é obrigatório!" }
+
+    }
+    else if(pet.hasOwnProperty('especie') == false){
+        return { erro: true, mensagem: "Campo 'especie' é obrigatório!" }
+
+    }
+    else if(pet.idade < 0){
+        return {erro: true, mensagem: "Campo 'idade' deve ser maior ou igual a zero!"}
+    }
+
+    else if(pet.sexo != ('M' || 'F')){
+        return {erro: true, mensagem: "Campo 'sexo' deve ser 'M' ou 'F'!"}
+    }
+    else{
+        pet.id = data.length + 1;
+        data.push(pet);
+        return pet;
+    } }
+    
+function editarPorId(id, pet){
+    const index = data.findIndex(pet => pet.id === id);
+    if (index === -1){
+        return { erro: true, mensagem: "Pet não encontrado!" }
+    } else if(pet.idade < 0){
+        return{erro: true, mensagem: "Campo 'idade' deve ser maior ou igual a zero!"}
+    } else if(pet.sexo != ('M' || 'F')) {
+        return{erro: true, mensagem: "Campo 'sexo' deve ser 'M' ou 'F'!"}
+    }
+    else{
+        pet.id = id 
+         data[index] = pet
+         return data[index]
+    }
+}
+
+function deletarPorId(id) {
+    const index = data.findIndex(pet => pet.id === id);
+    if(index === -1){ return { erro: true, mensagem: "Pet não encontrado!" }
+    } else {
+        data.splice(index , 1 )
+        return {
+            erro: false, mensagem: "Pet deletado com sucesso!"
+        }
+    }
+}
 
 module.exports = {
-    listar:listar,
-    listarPorId:listarPorId,
-    cadastrar:cadastrar,
-    cadastrar1:cadastrar1
+    listar,
+    listarPorId,
+    cadastrar,
+    editarPorId,
+    deletarPorId
 }
--- pets.test.js
+++ pets.test.js
@@ -1,57 +1,151 @@
 const pets = require("./pets");
 
-describe("Teste de funcionalidade listar", () => {
-
-    //it ou test
-    test("Listar todos os pets", () => {
-        expect(pets.listar()).toEqual(
-            expect.arrayContaining([
-                expect.objectContaining({
-                    id: expect.any(Number),
-                    nome: expect.any(String),
-                    idade: expect.any(Number),
-                    sexo: expect.any(String),
-                    especie: expect.any(String)
-                })
-            ])
-        )
-
+describe("Cadastramento de pets", () => {
+    it("Cadastrar novo pet", () => {
+        const length = pets.listar().length;
+        expect(pets.cadastrar({
+            nome: "Vitor",
+            idade: 10,
+            sexo: "M",
+            especie: "Gato"
+        })).toEqual(
+            expect.objectContaining({
+                id: expect.any(Number),
+                nome: expect.any(String),
+                idade: expect.any(Number),
+                sexo: 'M' || 'F', // M ou F, apenas
+                especie: expect.any(String)
+            })
+        );
+        // garantir que foi adicionado mais 1 ao banco de pets
+        expect(pets.listar().length).toEqual(length+1);
     });
 
-    it("Listar pet por ID", () => {
-        expect(pets.listarPorId(2)).toEqual(
+    it("Cadastrar - Campo 'nome' é obrigatório", () => {
+        const resultado = pets.cadastrar({ idade: 10, sexo: "M", especie: "Gato" });
+        expect(resultado).toEqual(expect.objectContaining({
+                erro: true,
+                mensagem: "Campo 'nome' é obrigatório!"
+            })
+        );
+    });
+
+    it("Cadastrar - Campo 'idade' é obrigatório", () => {
+        const resultado = pets.cadastrar({ nome: "Catz", sexo: "M", especie: "Gato" });
+        expect(resultado).toEqual(expect.objectContaining({
+                erro: true,
+                mensagem: "Campo 'idade' é obrigatório!"
+            })
+        );
+    });
+
+    it("Cadastrar - Campo 'sexo' é obrigatório", () => {
+        const resultado = pets.cadastrar({ nome: "Catz", idade: 10, especie: "Gato" });
+        expect(resultado).toEqual(expect.objectContaining({
+                erro: true,
+                mensagem: "Campo 'sexo' é obrigatório!"
+            })
+        );
+    });
+
+    it("Cadastrar - Campo 'espécie' é obrigatório", () => {
+        const resultado = pets.cadastrar({ nome: "Catz", idade: 10, sexo: "M" });
+        expect(resultado).toEqual(expect.objectContaining({
+                erro: true,
+                mensagem: "Campo 'especie' é obrigatório!"
+            })
+        );
+    });
+
+    it("Cadastrar - campo 'idade' deve ser maior ou igual a 0", () => {
+        pet = { nome: "Zeus", idade: -1, sexo: 'M', especie: 'cachorro'};
+        expect(pets.cadastrar(pet)).toEqual(
             expect.objectContaining({
-                id:2,
+                erro: true,
+                mensagem: "Campo 'idade' deve ser maior ou igual a zero!"
+            })
+        );
+    });
+
+    it("Cadastrar - campo 'sexo' deve ser 'M' ou 'F'", () => {
+        pet = { nome: "Zeus", idade: 5, sexo: 'Masculino', especie: 'cachorro'};
+        expect(pets.cadastrar(pet)).toEqual(
+            expect.objectContaining({
+                erro: true,
+                mensagem: "Campo 'sexo' deve ser 'M' ou 'F'!"
+            })
+        );
+    });
+});
+
+describe("Edição de pets por id", () => {
+
+    it("Editar pet por id", () => {
+        const edicao = pets.editarPorId(2, { 
+            nome: "Totó", idade: 2,
+            sexo: "M", especie: "cachorro"
+        });
+        
+        expect(edicao).toEqual(
+            expect.objectContaining({
+                id: expect.any(Number),
                 nome: expect.any(String),
                 idade: expect.any(Number),
                 sexo: expect.any(String),
-                especie: expect.any(String),
+                especie: expect.any(String)
+            })
+        );
+        
+        expect(pets.listarPorId(edicao.id)).toEqual(expect.objectContaining({ 
+            nome: "Totó", idade: 2,
+            sexo: "M", especie: "cachorro"
+        }));
+    });
+
+    it("Editar - pet não encontrado", () => {
+        expect(pets.editarPorId(999999, {
+            nome: "Totó",
+            idade: 2,
+            sexo: "M",
+            especie: "cachorro"
+        })).toEqual(
+            expect.objectContaining({
+                erro: true,
+                mensagem: "Pet não encontrado!"
             })
         );
     });
 
-    it("Listar por ID - Pet não encontrado", () => {
-        expect(pets.listarPorId(999999)).toEqual(
+    it("Editar - campo 'idade' deve ser maior ou igual a 0", () => {
+        pet = { nome: "Zeus", idade: -1, sexo: 'M', especie: 'cachorro'};
+        expect(pets.editarPorId(1, pet)).toEqual(
             expect.objectContaining({
-                erro: "True",
-                mensagem:"Pet não encontrado!"
+                erro: true,
+                mensagem: "Campo 'idade' deve ser maior ou igual a zero!"
             })
         );
     });
 
-    it("Cadastrar novo pet", () => {
-        expect(pets.cadastrar({ nome:"Gertrudi", idade:12, sexo:"F", especie:"Mula"})).toEqual(
-            expect.objectContaining({nome:"Gertrudi", idade:12, sexo:"F", especie:"Mula"})
-        );
-    });
-
-    it("Cadastrar", () => {
-        expect(pets.cadastrar1({ nome:"Júlia"})).toEqual(
+    it("Editar - campo 'sexo' deve ser 'M' ou 'F'", () => {
+        pet = { nome: "Zeus", idade: 5, sexo: 'Masculino', especie: 'cachorro'};
+        expect(pets.editarPorId(1, pet)).toEqual(
             expect.objectContaining({
-                mensagem: "Campo 'nome' é obrigatorio"
+                erro: true,
+                mensagem: "Campo 'sexo' deve ser 'M' ou 'F'!"
             })
         );
     });
 
 });
 
+describe("Deleção de pets", () => {
+
+    it("Deletar - Pet não encontrado", () => {
+        expect(pets.deletarPorId(9999999)).toEqual(
+            expect.objectContaining({
+                erro: true,
+                mensagem: "Pet não encontrado!"
+            })
+        )
+    });
+});
